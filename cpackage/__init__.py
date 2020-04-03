2d9cdff1d56a2edad4801efb410a0ade45344160
Cru1seControl <cru1secontrol.loot@gmail.com>
2020-04-03 16:55:38 -0400

    Docs, author, version added

--- cpackage/__init__.py
+++ cpackage/__init__.py
@@ -1,3 +1,20 @@
+"""
+A python utility for creating better modules/packages.
+This module give the ability to quickly create and add files to different directories.
+From creating directory & sub directory to managing the files that are put into those folders.
+This is more of an automation tool for writing modules but can be used for other package related matters.
+
+Example:
+    >>> from cpackage import CPack, CAccess
+    >>> CPack("mymodule", True, "code")
+    >>> CAccess("mymodule/code", True, "example.py")
+"""
+
+__all__     = ["CPack", "Cdir", "CAccess", "CRemove"]
+__author__  = "Cru1seControl <Cru1seControl.loot@gmail.com>"
+__version__ = "v1.0.0"
+
+
 try: #Ignore errors, just dont run this file!
     from cpackage.packaging import CPack
     from cpackage.packaging import Cdir
@@ -6,5 +23,3 @@ try: #Ignore errors, just dont run this file!
 
 except ImportError as importerror:
     print(importerror)
-
-__all__ = ["CPack", "Cdir", "CAccess", "CRemove"]
