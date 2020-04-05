dac002539e6d4d19524c015059b4c687237c5fbf
Cru1seControl <cru1secontrol.loot@gmail.com>
2020-04-05 13:42:30 -0400

    Added args.volume & args.volumes

--- cpackage/tool.py
+++ cpackage/tool.py
@@ -2,7 +2,7 @@ try:
     import argparse
     import os
     import sys
-    from cpackage.packaging import Cdir, CRemove
+    from cpackage.packaging import Cdir, CRemove, CVolume
 
 except ImportError as importerror:
     print(importerror)
@@ -13,6 +13,8 @@ parser.add_argument("-sub", help="Sub package names to create", nargs="+", type=
 parser.add_argument("-listdir", help="List directory and sub packages")
 parser.add_argument("-access", help="Create files within a package")
 parser.add_argument("-remove", help="Remove entire package tree")
+parser.add_argument("-volume", help="View the current package volume size")
+parser.add_argument("-volumes", help="Add two or more volume sizes together", nargs="+", type=str)
 
 parser.add_argument("--site", help="Use python site packages directory", action="store_true")
 parser.add_argument("--current", help="Use current working directory", action="store_true")
@@ -26,7 +28,7 @@ class _toolConstruct(object):
         elif args.site:
             return False
 
-    # has potential future use 
+    # has potential future use
     def subTup():
         argsubLst = []
         if args.sub:
@@ -80,5 +82,13 @@ def tool():
         else:
             CRemove(args.remove, False)
 
-tool()
+    if args.volume:
+        print(CVolume(args.volume))
 
+    if args.volumes:
+        size = 0
+        for vols in args.volumes:
+            size += CVolume(str(vols))[0]
+
+        print(size)
+tool()
